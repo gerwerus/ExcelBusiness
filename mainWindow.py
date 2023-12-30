02cfadf9d74c231b2109e0917f419f6ff5d02c4f
= <prosyannikov1978@mail.ru>
2023-12-30 19:52:50 +0500

    Add getFilesButton

--- mainWindow.py
+++ mainWindow.py
@@ -5,4 +5,17 @@ from PySide6.QtWidgets import (
     QDialogButtonBox, QMessageBox, QTabWidget, QScrollArea
     )
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.setWindowTitle("Excel Plan")
+        self.setFixedSize(600, 400)
+        layout = QGridLayout()
+        self.getFilesButton = QPushButton("Загрузить файлы")
+        self.getFilesButton.clicked.connect(self.getFiles)
+        layout.addWidget(self.getFilesButton, 0, 0)
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+    def getFiles(self):
+        files = QFileDialog.getOpenFileNames(self, filter="Excel files (*.xls, *xlsx);; All files (*)")[0]  
+        print(files)
