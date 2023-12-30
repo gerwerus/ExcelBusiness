9f2e00c6b950567afca72d1b735ea99c46a1ae1e
= <prosyannikov1978@mail.ru>
2023-12-30 22:06:32 +0500

    Add Tread to excel Proccess, Parse Institute Name

---
+++ excelProcessing.py
@@ -0,0 +1,26 @@
+from PySide6.QtCore import QThread
+from openpyxl import load_workbook
+class ExcelTread(QThread):
+    def __init__(self, mainWindow):
+        super().__init__()
+        self.mainWindow = mainWindow
+        self.objects = 0
+        self.ln = 0
+    def setObjects(self, objects, ln=0):
+        self.objects = objects
+        self.ln = ln
+
+    def run(self):
+        for index, obj in enumerate(self.objects):
+            print(obj.getInstituteName())
+            self.mainWindow.progressBar.setValue(int(((index + 1)/self.ln) * 100)) 
+class ExcelProccessing():
+    def __init__(self, filename):
+        self.filename = filename
+        self.book = load_workbook(filename)
+        self.titleList = self.book['Титул']
+        self.instituteName = self.titleList['D38'].value
+    def getInstituteName(self):
+        return self.instituteName
+        
+      
--- mainWindow.py
+++ mainWindow.py
@@ -1,21 +1,34 @@
+from excelProcessing import ExcelProccessing, ExcelTread
+import time
 from PySide6.QtWidgets import (
     QLineEdit, QPushButton, QApplication, QTextEdit,
     QVBoxLayout, QDialog, QGroupBox, QMainWindow, QRadioButton,
     QFileDialog, QWidget, QDateEdit, QLabel, QGridLayout,
-    QDialogButtonBox, QMessageBox, QTabWidget, QScrollArea
+    QDialogButtonBox, QMessageBox, QTabWidget, QScrollArea, QProgressBar
     )
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__()
         self.setWindowTitle("Excel Plan")
         self.setFixedSize(600, 400)
+        self.files = []
         layout = QGridLayout()
         self.getFilesButton = QPushButton("Загрузить файлы")
+        self.progressBar = QProgressBar()
+        self.progressBar.setValue(0) 
         self.getFilesButton.clicked.connect(self.getFiles)
         layout.addWidget(self.getFilesButton, 0, 0)
+        layout.addWidget(self.progressBar, 1, 0)
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
+        self.excelTread_instance = ExcelTread(self)
     def getFiles(self): 
-        files = QFileDialog.getOpenFileNames(self, filter="Excel files (*.xls, *xlsx);; All files (*)")[0]  
-        print(files)
+        self.files = QFileDialog.getOpenFileNames(self, filter="Excel files (*.xls, *xlsx);; All files (*)")[0]
+        ln = len(self.files) 
+        objects = (ExcelProccessing(i) for i in self.files) 
+        self.excelTread_instance.setObjects(objects, ln)
+        self.excelTread_instance.start()  
+        # for index, obj in enumerate(objects):
+        #     print(obj.getInstituteName())
+        #     self.progressBar.setValue(int(((index + 1)/l) * 100)) 
