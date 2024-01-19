8b70168ea9eef39fe554d6b97f1d6ba7b6976006
= <prosyannikov1978@mail.ru>
2024-01-19 16:23:13 +0700

    Parse Practice Type And Sem

--- excelProcessing.py
+++ excelProcessing.py
@@ -11,15 +11,17 @@ class ExcelTread(QThread):
         self.ln = ln
 
     def run(self):
-        print(self.mainWindow.dateEdit.date())
+        # print(self.mainWindow.dateEdit.date())
         for index, obj in enumerate(self.objects):
-            print(obj.getInstituteName(), obj.getCode(), obj.getProfile(), obj.getDateStart())
+            # print(obj.getInstituteName(), obj.getCode(), obj.getProfile(), obj.getDateStart())
+            obj.getView()
             self.mainWindow.progressBar.setValue(int(((index + 1)/self.ln) * 100)) 
 class ExcelProccessing():
     def __init__(self, filename):
         self.filename = filename
         self.book = load_workbook(filename)
         self.titleList = self.book['Титул']
+        self.practiceList = self.book['Практики']
     def getInstituteName(self):
         return self.titleList['D38'].value # С заглавной??? 
     def getCode(self):
@@ -28,5 +30,25 @@ class ExcelProccessing():
         return self.titleList['D30'].value
     def getDateStart(self):
         return self.titleList['W40'].value
-        
+    def getView(self):
+        practiceView = []
+        practiceTyp = []
+        practiceSem = []
+        for i in range(3, self.practiceList.max_row + 1):
+            view = self.practiceList['A' + str(i)].value
+            typ = self.practiceList['B' + str(i)].value
+            course = self.practiceList['D' + str(i)].value
+            sem = self.practiceList['E' + str(i)].value
+            if view:
+                if "Вид" in view:
+                    practiceView.append(view[14:])
+            if typ:
+                practiceTyp.append(typ)
+            if course:
+                practiceSem.append((int(course) - 1) * 2 + sem) 
+        practiceList = []
+        for i in range(len(practiceView)):
+            practiceList.append([practiceView[i], practiceTyp[i], practiceSem[i]])
+        print(practiceList)
+        return practiceList        
       
