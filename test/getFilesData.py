70668c27370d68851f8733a897d9987e5e80d838
= <prosyannikov1978@mail.ru>
2024-02-24 14:41:32 +0700

    Fix getFilesData

--- test/getFilesData.py
+++ test/getFilesData.py
@@ -3,6 +3,7 @@ def getFilesData(planPath="plans"):
     data = []
     for top, _, files in os.walk(planPath):
         for file in files:
-            data.append(os.path.join(top, file))
+            if file.endswith(".xls") or file.endswith(".xlsx"):
+                data.append(os.path.join(top, file))
     return data
 print(getFilesData("plans"))
