8b10a5fec89e3c8573f6b93bde215d9bd2db78a4
nekit22713 <pentak228@mail.ru>
2024-02-26 12:49:00 +0700

    add_newfunc_tablecomponent

--- components/table.py
+++ components/table.py
@@ -1,4 +1,5 @@
 from excelProcessing import ExcelProccessing, ExcelTread
+from openpyxl import Workbook
 from PySide6.QtCore import QDate
 from PySide6.QtWidgets import (
     QLineEdit, QPushButton, QApplication, QTextEdit,
@@ -23,3 +24,29 @@ class TableComponent(QTableWidget):
         self.insertRow(row)
         for index, el in enumerate(line):
             self.setItem(row, index, QTableWidgetItem(str(el)))
+    
+    def clearTable(self):
+        self.setRowCount(0)
+
+    def exportToExcel(self, filename: str = "table_data.xlsx"):
+        workbook = Workbook()
+        sheet = workbook.active
+
+        for columnIndex, header in enumerate(self.header, start=1):
+            sheet.cell(row=1, column=columnIndex, value=header)
+
+        for rowIndex, rowData in enumerate(self.get_all_data(), start=2):
+            for columnIndex, cellValue in enumerate(rowData, start=1):
+                sheet.cell(row=rowIndex, column=columnIndex, value=cellValue)
+
+        workbook.save(filename)
+
+    def get_all_data(self):
+        data = []
+        for row in range(self.rowCount()):
+            rowData = []
+            for column in range(self.columnCount()):
+                item = self.item(row, column)
+                rowData.append(item.text() if item else "")
+            data.append(rowData)
+        return data
